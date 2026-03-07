b26060399d4185e605beda14f6b25fe25402df05
Aditya sakare <aditya.sakare16@gmail.com>
2026-03-08 00:00:05 +0530

    Server: WebSocket added

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,18 @@
 const http = require('http')
 const server = http.createServer();
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
 
 server.listen(57778, () => {
     console.log('[Server] Listening on port 57778');
 });
+
+wss.on('connection', (conn) =>{
+    console.log('[server] New Connection');
+
+    conn.on('message', (data) => {
+        const msg = JSON.parse(data);
+        console.log('[server] Connection closed');
+    });
+});
+
