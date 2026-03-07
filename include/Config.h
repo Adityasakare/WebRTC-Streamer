758e6ae40cd4d8ad9d97695af82d41d5db33edb7
Aditya sakare <aditya.sakare16@gmail.com>
2026-03-07 21:30:39 +0530

    Application Configuration file added

---
+++ include/Config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ====================== Server & Camera Settings ======================
+#define DEFAULT_SERVER_URL      "http://127.0.0.1:57778"
+#define DEFAULT_CONFIG_PATH     "cameras.conf"
+
+#define STUN_SERVER "stun://stun.l.google.com:19302"
+
+// ====================== Video Settings ======================
+#define VIDEO_WIDTH         640
+#define VIDEO_HEIGHT        480
+#define VIDEO_FRAMERATE     30
+
+// ====================== Recording Settings ======================
+#define RECORDING_DIR           "recordings/"
+#define RECORDING_CHUNKS_SECS   30
+
+
+
+
+#endif
