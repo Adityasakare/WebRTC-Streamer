6d54843b02cc6468ac871a83bd3e358e5bb5e5ca
Aditya sakare <aditya.sakare16@gmail.com>
2026-03-08 19:21:22 +0530

    main: driver code modified

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,64 @@
 #include <gst/gst.h>
+#include <glib.h>
 #include <csignal>
+
 #include "../include/Logger.h"
+#include "../include/Config.h"
+#include "../include/WebRTCApp.h"
 
-static GMainLoop* g_loop = nullptr;
+static WebRTCApp* g_app = nullptr;
 
-static gboolean onBusMessage(GstBus*, GstMessage* msg, gpointer)
-{
-    switch (GST_MESSAGE_TYPE(msg)) 
-    {
-        case GST_MESSAGE_ERROR: 
-        {
-            GError* err = nullptr;
-            gchar*  dbg = nullptr;
-            gst_message_parse_error(msg, &err, &dbg);
-            Logger::getInstance().log(LogLevel::ERROR,
-                "Pipeline error: %s | %s", err->message, dbg ? dbg : "");
-            g_error_free(err);
-            g_free(dbg);
-            if (g_loop) 
-                g_main_loop_quit(g_loop);
-            break;
-        }
-        case GST_MESSAGE_EOS:
-            Logger::getInstance().log(LogLevel::INFO, "Pipeline EOS");
-            if (g_loop) 
-                g_main_loop_quit(g_loop);
-            break;
-        default:
-            break;
-    }
-    return TRUE;
-}
+static const gchar* opt_server = DEFAULT_SERVER_URL;
+static const gchar* opt_user = "streamer";
+static const gchar* opt_config = DEFAULT_CONFIG_PATH;
 
-
-
-static void buildRecordPipeline(const std::string& device)
-{
-    GError* err = nullptr;
-
-    std::string desc =
-        "v4l2src device=" + device + " do-timestamp=true ! "
-        "videoconvert ! videoscale ! "
-        "video/x-raw,width=640,height=480,framerate=30/1 ! "
-        "x264enc bitrate=1000 speed-preset=ultrafast tune=zerolatency key-int-max=30 ! "
-        "video/x-h264,profile=baseline ! "
-        "h264parse config-interval=-1 ! "
-        "mp4mux ! "
-        "filesink location=/tmp/test_record.mp4";
-
-    GstElement* pipeline = gst_parse_launch(desc.c_str(), &err);
-
-    if (err) 
-    {
-        Logger::getInstance().log(LogLevel::ERROR, "Pipeline error: %s", err->message);
-        g_error_free(err);
-        return;
-    }
-
-    GstBus* bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
-    gst_bus_add_watch(bus, onBusMessage, nullptr);
-    gst_object_unref(bus);
-
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    Logger::getInstance().log(LogLevel::INFO, "Recording for 5 seconds...");
-
-    g_main_loop_run(g_loop);
-
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
-
-    Logger::getInstance().log(LogLevel::INFO, "Done. Check /tmp/test_record.mp4");
-}
-
-
+static GOptionEntry cli_entries[] = {
+    { "server", 's', 0, G_OPTION_ARG_STRING, &opt_server, "Server URL", "URL"  },
+    { "user",   'u', 0, G_OPTION_ARG_STRING, &opt_user,   "Username",   "NAME" },
+    { "config", 'c', 0, G_OPTION_ARG_STRING, &opt_config, "Config file","FILE" },
+    { nullptr }
+};
 
 static void onSignal(int)
 {
-    Logger::getInstance().log(LogLevel::INFO, "Interrupt — stopping...");
-    if (g_loop) 
-        g_main_loop_quit(g_loop);
+    Logger::getInstance().log(LogLevel::INFO, "Shutting down...");
+    if (g_app) 
+    {
+        delete g_app;
+        g_app = nullptr;
+        exit(0);
+    }
 }
+
 
 
 int main(int argc, char* argv[])
 {
-    gst_init(&argc, &argv);
+    GOptionContext* ctx = g_option_context_new(" WebRTC Streamer");
+    g_option_context_add_main_entries(ctx, cli_entries, nullptr);
+    g_option_context_add_group(ctx, gst_init_get_option_group());
+
+    GError* err = nullptr;
+    if (!g_option_context_parse(ctx, &argc, &argv, &err)) 
+    {
+        Logger::getInstance().log(LogLevel::ERROR, "%s", err->message);
+        return 1;
+    }
     
+    g_option_context_free(ctx);
+
     std::signal(SIGINT,  onSignal);
     std::signal(SIGTERM, onSignal);
 
-    g_loop = g_main_loop_new(nullptr, FALSE);
+    Logger::getInstance().log(LogLevel::INFO, "== WebRTC Streamer ==");
+    Logger::getInstance().log(LogLevel::INFO, "Server: %s", opt_server);
+    Logger::getInstance().log(LogLevel::INFO, "User:    %s", opt_user);
+    Logger::getInstance().log(LogLevel::INFO, "Config: %s", opt_config);
 
-    buildRecordPipeline("/dev/video0");
+    g_app = new WebRTCApp(opt_config, opt_server, opt_user);
+    g_app->run();
 
-    gst_deinit();
-
+    delete g_app;
     return 0;
 }
 
